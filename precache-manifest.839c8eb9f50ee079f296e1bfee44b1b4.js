1482d6fa6e7b4625b5f29273901219e731dffbc8
Pythonsway <adam@pythonsway.it>
2019-08-15 08:06:03 +0200

    Updates

---
+++ precache-manifest.839c8eb9f50ee079f296e1bfee44b1b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648e5cbed4f1f5b2398c2a0209d28a61",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7a9fa1223c979426a1d4",
+    "url": "/calculator/static/css/main.865ece2f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/calculator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7a9fa1223c979426a1d4",
+    "url": "/calculator/static/js/main.673df18b.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
